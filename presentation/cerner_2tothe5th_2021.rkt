bd42a24cf192bd74f32b836b0d25bf208b25962a
Srinandan Pai <31960499+SrinandanPai@users.noreply.github.com>
2021-10-03 00:43:56 +0530

    Program to create slides for 2^5 Coding Competition

---
+++ presentation/cerner_2tothe5th_2021.rkt
@@ -0,0 +1,20 @@
+#lang slideshow
+
+; cerner_2tothe5th_2021
+; Program to create slides for 2^5 Coding Competition
+
+(slide
+ #:title "2^5 Coding Competition 2021"
+ (t "Engineering Collaboration"))
+(slide
+ #:title "2^5 Coding Competition 2021"
+ (t "Categories")
+ 'next
+ (item "Best Representation")
+ 'next
+ (item "Most Obfuscated")
+ 'next
+ (item "Greatest Variety in Language"))
+(slide
+ #:title "2^5 Coding Competition 2021"
+ (t "Thank You"))
